ef1e1635fc15605a0852413bdea0bc79e02d4c02
cupokki <nd1346@gmail.com>
2025-06-24 23:12:42 +0900

    feat: 베이스 예외

--- src/main/java/dev/cupokki/auth/exception/ExceptionAdviceHandler.java
+++ src/main/java/dev/cupokki/auth/exception/ExceptionAdviceHandler.java
@@ -1,6 +1,7 @@
 package dev.cupokki.auth.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +12,13 @@ public class ExceptionAdviceHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
-        log.warn("임시 에러 핸들러");
+        log.warn(e.getLocalizedMessage());
         return ResponseEntity.badRequest().body(e.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> AuthenticationException(UserAuthenticationException e) {
+        log.warn(e.getMessage());
+        return ResponseEntity.status(e.getHttpStatusCode()).body(e.getMessage());
     }
 }
