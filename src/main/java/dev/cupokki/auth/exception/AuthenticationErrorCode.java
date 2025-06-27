e2898e2d991d2f59ad4bf6b62c0d5e0a5fb90e97
cupokki <nd1346@gmail.com>
2025-06-27 23:33:19 +0900

    feat: Jwt 커스텀 예외 추가

--- src/main/java/dev/cupokki/auth/exception/AuthenticationErrorCode.java
+++ src/main/java/dev/cupokki/auth/exception/AuthenticationErrorCode.java
@@ -11,7 +11,11 @@ import org.springframework.http.ProblemDetail;
 public enum AuthenticationErrorCode {
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "사용할 수 없는 이메일입니다."),
     INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "이메일 또는 비밀번호가 일치하지않습니다."),
-    CONFIRM_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호 확인이 일치하지 않습니다.");
+    CONFIRM_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호 확인이 일치하지 않습니다."),
+    INVALID_TOKEN_SIGNATURE(HttpStatus.UNAUTHORIZED, "토큰 서명이 올바르지 않습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    UNSUPPORTED_TOKEN_FORMAT(HttpStatus.UNAUTHORIZED, "잘못된 토큰 포멧 입니다."),
+    INVALID_TOKEN_VALUE(HttpStatus.UNAUTHORIZED, "유효한 토큰 값이 아닙니다.");
 
     private HttpStatus httpStatus;
     private String message;
